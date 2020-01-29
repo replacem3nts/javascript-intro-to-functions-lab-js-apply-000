c1be72b244bc88906c2534d1cbedf0f3fcd4a079
Donny Landis <donny.landis@gmail.com>
2020-01-29 20:18:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+  return string.toUpperCase('Hello')
+}
+function whisper(string) {
+  return string.toLowerCase('HELLO')
+}
+function logShout(string) {
+  console.string.toUpperCase('hello')
+}
